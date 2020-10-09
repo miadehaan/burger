250414390c39e979c353d89d41b391fd185701ab
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-10-08 22:50:50 -0700

    files added & directory structure setup

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+/* 
+Setup the code to connect Node to MySQL.
+Export the connection.
+*/
+var mysql = require("mysql");
+
+
+module.exports = ;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT burgers (burger_name, devoured) VALUES ("Veggie Burger", 1);
+INSERT burgers (burger_name, devoured) VALUES ("Cheese Burger", 0);
+INSERT burgers (burger_name, devoured) VALUES ("Turkey Burger", 0);

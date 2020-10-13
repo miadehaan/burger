3e5cef30494a80a9d34225546b0563703bc64bb4
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-10-12 17:46:22 -0700

    delete function & create functions now work

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,25 @@ Setup the code to connect Node to MySQL.
 Export the connection.
 */
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL); 
+}
+else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'Webdev_2020!!',
+        database: 'burgers_db'
 
-module.exports = ;
+        // Heroku: ???
+        // host: 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        // user: 'l5gc06n1hn8vxqrn',
+        // password: 'ojl273mtrmraei90',
+        // database: 'burgers_db'
+    })
+}
+
+connection.connect();
+module.exports = connection;
--- models/burger.js
+++ models/burger.js
@@ -6,4 +6,31 @@
     * Export at the end of the `burger.js` file.
 
 */
-var orm = require("./config/orm.js");
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(callback) {
+        orm.all("burgers", function(res){
+            callback(res);
+        });
+    },
+    create: function(cols, vals, callback) {
+        orm.create("burgers", cols, vals, function(res) {
+            callback(res);
+            
+        });
+    },
+    update: function(objColVals, condition, callback) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            callback(res);
+        });
+    },
+    delete: function(condition, callback) {
+        orm.delete("burgers", condition, function(res) {
+            callback(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger; 
